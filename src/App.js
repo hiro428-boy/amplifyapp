4f80f3b51ab6a5a5d8ccc7880a3d5ea95df644ac
hiro428-boy <h.tgaoye42@gmail.com>
2022-04-26 23:35:06 +0900

    fixed

--- src/App.js
+++ src/App.js
@@ -1,91 +1,101 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 // import logo from './logo.svg';
-import './App.css';
-import { API, Storage } from 'aws-amplify';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react-v1'
-import "@aws-amplify/ui-react/styles.css"
-import { listNotes } from './graphql/queries';
-import { createNote as createNoteMutation, deleteNote as deleteNoteMutation } from './graphql/mutations';
+import "./App.css";
+import { API, Storage } from "aws-amplify";
+import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react-v1";
+import "@aws-amplify/ui-react/styles.css";
+import { listNotes } from "./graphql/queries";
+import {
+  createNote as createNoteMutation,
+  deleteNote as deleteNoteMutation,
+} from "./graphql/mutations";
 
-const initialFormState = { name: '', description: '' };
+const initialFormState = { name: "", description: "" };
 function App() {
   const [notes, setNotes] = useState([]);
   const [formData, setFormData] = useState(initialFormState);
 
   useEffect(() => {
     fetchNotes();
-  }, [])
+  }, []);
 
   //APIクラスを使用してクエリをGraphQL APIに送信し、メモのリストを取得します
   const fetchNotes = async () => {
     const apiData = await API.graphql({ query: listNotes });
     const notesFromAPI = apiData.data.listNotes.items;
     setNotes(apiData.data.listNotes.items);
-    await Promise.all(notesFromAPI.map(async note => {
-      if (note.image) {
-        const image = await Storage.get(note.image);
-        note.image = image;
-      }
-    }))
+    await Promise.all(
+      notesFromAPI.map(async (note) => {
+        if (note.image) {
+          const image = await Storage.get(note.image);
+          note.image = image;
+        }
+      })
+    );
     setNotes(apiData.data.listNotes.items);
-  }
+  };
 
   //APIクラスを使用してクエリをGraphQL APIに送信しする。GraphQL API変異に必要な変数を渡して、フォームデータで新しいノートを作成できる
-  const createNote = async() => {
+  const createNote = async () => {
     if (!formData.name || !formData.description) return;
-    await API.graphql({ query: createNoteMutation, variables: { input: formData } });
+    await API.graphql({
+      query: createNoteMutation,
+      variables: { input: formData },
+    });
     if (formData.image) {
       const image = await Storage.get(formData.image);
       formData.image = image;
     }
     setNotes([...notes, formData]);
     setFormData(initialFormState);
-  }
+  };
 
   //変数と共にGraphQLミューテーションを送信しますが、メモを作成する代わりにメモを削除します
-  const deleteNote = async ({id}) => {
-    const newNotesArray = notes.filter(note => note.id !== id);
+  const deleteNote = async ({ id }) => {
+    const newNotesArray = notes.filter((note) => note.id !== id);
     setNotes(newNotesArray);
-    await API.graphql({ query: deleteNoteMutation, variables: { input: { id } } });
-    
-  }
+    await API.graphql({
+      query: deleteNoteMutation,
+      variables: { input: { id } },
+    });
+  };
   const onChange = async (e) => {
     if (!e.target.files[0]) return;
     const file = e.target.value;
     setFormData({ ...formData, image: file.name });
     await Storage.put(file.name, file);
     fetchNotes();
-  }
+  };
   return (
     <div className="App">
       <h1>My Notes App</h1>
       <input
-        onChange={(e) => setFormData({ ...formData, 'name': e.target.value })}
-        placeholder='Note name'
+        onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+        placeholder="Note name"
         value={formData.name}
       />
       <input
-        onChange={(e) => setFormData({ ...formData, 'description': e.target.value })}
-        placeholder='Note description'
+        onChange={(e) =>
+          setFormData({ ...formData, description: e.target.value })
+        }
+        placeholder="Note description"
         value={formData.description}
       />
-      <input type='file' onChange={onChange} />
+      <input type="file" onChange={onChange} />
       <button onClick={createNote}>Create Note</button>
-      <div style={{marginBottom: 30}}>
-        {
-          notes.map(note => (
-            <div key={note.id || note.name}>
-              <h2>{note.name}</h2>
-              <p>{note.description}</p>
-              <button onClick={() => deleteNote(note)}>Delete note</button>
-              {
-                note.image && <img src={note.image} style={{width: 400}} alt='' />
-              }
-            </div>
-          ))
-        }
+      <div style={{ marginBottom: 30 }}>
+        {notes.map((note) => (
+          <div key={note.id || note.name}>
+            <h2>{note.name}</h2>
+            <p>{note.description}</p>
+            <button onClick={() => deleteNote(note)}>Delete note</button>
+            {note.image && (
+              <img src={note.image} style={{ width: 400 }} alt="" />
+            )}
+          </div>
+        ))}
       </div>
-      <AmplifySignOut/>
+      <AmplifySignOut />
     </div>
   );
 }
